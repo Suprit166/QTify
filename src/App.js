58e0d91cb83eb7b785625e1a5c50fd75098fbdca
Suprit Keni <supritkeni@gmail.com>
2023-11-13 17:46:20 +0530

    confirm

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
+import Hero_Section from "./components/Hero_Section/HeroSection";
 
 
 function App() {
   return (
     <>
       <Navbar/>
+      <Hero_Section/>
     </>
   );
 }
---
+++ src/components/Hero_Section/HeroSection.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "./Hero.section.module.css"
+
+export default function Hero_Section () {
+    return (
+        <div className={styles.hero}>
+            <div className={styles.bgImg}></div>
+        </div>
+    )
+}
+
